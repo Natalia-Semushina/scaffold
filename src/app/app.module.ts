d4e7df04f0390924a7f8f0132822de1b6d74a4fb
Natalia-Semushina <natusic431ka@gmail.com>
2020-08-07 12:12:09 +0300

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FlexLayoutModule, BrowserAnimationsModule, MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
